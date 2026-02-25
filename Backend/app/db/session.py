8d5e1ca027e57538773e09f8fd91eb497b4fdc89
JoeHeinz-ctrl <jaffijoe03@gmail.com>
2026-02-25 09:42:12 +0530

    Updated DB URL

--- Backend/app/db/session.py
+++ Backend/app/db/session.py
@@ -1,15 +1,14 @@
-from sqlalchemy.orm import sessionmaker
-from app.db.database import engine
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}  # REQUIRED for SQLite
 )
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
